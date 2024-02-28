c2ccec268df957d4834c758bb7c92023e97ce5be
Kulsrestha Joshi <128225718+Kulsrestha@users.noreply.github.com>
2024-02-28 10:35:56 +0530

    Add files via upload

---
+++ js/result.js
@@ -0,0 +1,17 @@
+let result = {
+    "tag": "",
+    "free": true,
+    "role": false,
+    "user": "kjkulsrestha2002",
+    "email": "kjkulsrestha2002@gmail.com",
+    "score": 0.64,
+    "state": "deliverable",
+    "domain": "gmail.com",
+    "reason": "valid_mailbox",
+    "mx_found": true,
+    "catch_all": null,
+    "disposable": false,
+    "smtp_check": true,
+    "did_you_mean": "",
+    "format_valid": true
+};
